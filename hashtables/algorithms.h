e30a3d879124d038dc25adacb8f7c20ea299acae
Joona Ylitalo <146531209+jypiter112@users.noreply.github.com>
2025-11-08 14:51:09 +0200

    Add binary search function to algorithms.h

---
+++ hashtables/algorithms.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <optional>
+#include <vector>
+
+/*
+    Returns the index of the target value
+*/
+template<typename type>
+int binarySearch(std::vector<type>& vector, type target){
+    int left { 0 };
+    int right { static_cast<int>(vector.size()) - 1 };
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(vector[mid] == target)
+            return mid;
+        if(vector[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    // no value
+    return -1;
+}
